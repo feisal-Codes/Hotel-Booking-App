6c21499e837d026cbe7cb1b62fe5a2823d2d31cb
feisal-Codes <feisalmib@gmail.com>
2022-09-20 23:20:49 +0300

    updates

--- util/http.js
+++ util/http.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-let url = "http://192.168.43.202:3001/rooms?type=";
+
 
 
 export async function getRoomsData(name) {
   try {
-    const response = await axios.get(url + name);
+    const response = await axios.get(url + "rooms?type=" + name);
 
     console.log(response.data.count);
     console.log("***************************************8");
@@ -21,3 +21,79 @@ export async function getRoomsData(name) {
     console.log("err", err);
   }
 }
+
+export async function postLogin({ email, password }) {
+  try {
+    console.log("This is the login Dta");
+    console.log(email, password);
+    const response = await axios.post(Loginurl, {
+      email: email,
+      password: password,
+    });
+    // console.log(response);
+    return response;
+  } catch (err) {
+    console.log("this is err");
+    console.log(err);
+    if (typeof err.response.data.message !== "undefined") {
+      let errObj = {
+        status: err.response.status,
+        message: err.response.data.message,
+      };
+
+      return errObj;
+    }
+  }
+}
+
+export async function postRegister({
+  firstname,
+  lastname,
+  email,
+  password,
+  phonenumber,
+}) {
+  try {
+    console.log("**********************************&&&&&&&&&&&&&");
+    console.log(firstname, lastname, email, phonenumber, password);
+    console.log("**********************************&&&&&&&&&&&&&");
+    const response = await axios.post(registerUrl, {
+      email: email,
+      password: password,
+      firstname: firstname,
+      lastname: lastname,
+      phoneNumber: phonenumber,
+    });
+    console.log(response);
+    return response;
+  } catch (err) {
+    console.log("this is err");
+    console.log(err);
+    console.log(err.response.data);
+    console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+    console.log(err.response.status);
+    console.log(err.response.data);
+    console.log("&&&$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+    console.log(typeof err.response.data);
+    console.log(typeof err.response);
+    if (typeof err.response.data !== "undefined") {
+      let errObj = {
+        status: err.response.status,
+        message: err.response.data.message,
+      };
+
+      return errObj;
+    }
+  }
+}
+
+export async function getRoomType() {
+  try {
+    const response = await axios.get(roomUrl);
+    console.log(response.data);
+   
+  } catch (err) {
+    console.log(err);
+  }
+}
+
